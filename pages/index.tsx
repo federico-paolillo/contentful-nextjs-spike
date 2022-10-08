4281b463bf194d51e125f63d0be87db0c4368c46
Paolillo <federico.paolillo@adesso.ch>
2022-10-08 21:54:29 +0200

    chore: initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contentful + NextJS Spike</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>A blog</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
