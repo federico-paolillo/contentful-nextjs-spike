c60587a4e2228cebbd7a6a9efa4b7b93762e19f2
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2022-10-10 22:49:13 +0200

    fix: added .end() to response

--- pages/api/posts/revalidate.ts
+++ pages/api/posts/revalidate.ts
@@ -5,18 +5,18 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     req.headers[process.env.REVALIDATE_KEY_HEADER_NAME];
 
   if (!maybeRevalidationToken) {
-    return res.status(400);
+    return res.status(400).end();
   }
 
   if (Array.isArray(maybeRevalidationToken)) {
-    return res.status(400);
+    return res.status(400).end();
   }
 
   if (maybeRevalidationToken !== process.env.REVALIDATE_KEY) {
-    res.status(401);
+    return res.status(401).end();
   }
 
   await res.revalidate("/index");
 
-  return res.status(204);
+  return res.status(204).end();
 }
