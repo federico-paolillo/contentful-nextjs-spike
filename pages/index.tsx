a61685c65e35ba2a5f594b6a9e9c89c42df30efe
NΛRCISO <16388025+413n@users.noreply.github.com>
2022-10-12 10:20:47 +0200

    feat: tailwind restyling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { GetStaticProps, NextPage } from "next";
+import Image from "next/future/image";
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
+import { BoxedLayout } from "../components/BoxedLayout";
 import { newContentfulClient } from "../contentful/client";
-import styles from "../styles/Home.module.css";
 
 interface BlogPostDetail {
   id: string;
   title: string;
+  teaser: string;
+  publicationDate: Date;
 }
 
 interface HomePageProps {
@@ -20,25 +22,75 @@ const contentfulClient = newContentfulClient(
 );
 
 const Home: NextPage<HomePageProps> = ({ blogPostDetails }) => {
+  const [firstBlogPostDetail, ...otherBlogPostDetails] = blogPostDetails;
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Contentful + NextJS Spike</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>A blog</h1>
-        <ul>
-          {blogPostDetails.map((blogPostDetail) => (
-            <li key={blogPostDetail.id}>
-              <Link href={`posts/${blogPostDetail.id}`}>
-                {blogPostDetail.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </div>
+
+      <BoxedLayout>
+        <div className="flex flex-col gap-20">
+          <h1 className="text-5xl font-bold text-gray-700 dark:text-gray-200 sm:text-7xl">
+            A blog ✨
+          </h1>
+
+          <Link href={`posts/${firstBlogPostDetail.id}`}>
+            <div className="flex cursor-pointer flex-col gap-4 sm:gap-8 md:flex-row lg:gap-12">
+              <div className="relative aspect-video h-auto w-full min-w-[50%] overflow-hidden rounded">
+                <Image src="/images/cover1.png" alt="Post image" fill />
+              </div>
+
+              <div className="flex flex-col gap-2 sm:gap-4">
+                <p className="text-sm font-medium text-gray-400 dark:text-gray-600">
+                  {new Date(
+                    firstBlogPostDetail.publicationDate
+                  ).toLocaleDateString("en-US")}
+                </p>
+
+                <h2 className="text-4xl font-semibold leading-tight text-gray-900 dark:text-gray-50 sm:text-5xl">
+                  {firstBlogPostDetail.title}
+                </h2>
+              </div>
+            </div>
+          </Link>
+
+          <ul className="grid grid-cols-12 gap-8">
+            {otherBlogPostDetails.map((blogPostDetail) => (
+              <li
+                key={blogPostDetail.id}
+                className="col-span-12 sm:col-span-6 md:col-span-4"
+              >
+                <Link href={`posts/${blogPostDetail.id}`}>
+                  <div className="flex cursor-pointer flex-col items-center gap-4">
+                    <div className="relative aspect-video h-auto w-full overflow-hidden rounded">
+                      <Image src="/images/cover2.png" alt="Post image" fill />
+                    </div>
+                    <div className="flex w-full flex-col gap-1.5">
+                      <p className="text-sm font-medium text-gray-400 dark:text-gray-600">
+                        {new Date(
+                          blogPostDetail.publicationDate
+                        ).toLocaleDateString("en-US")}
+                      </p>
+
+                      <h2 className="text-ellipsis text-xl font-semibold text-gray-900 dark:text-gray-50 sm:text-2xl">
+                        {blogPostDetail.title}
+                      </h2>
+
+                      <p className="text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-lg">
+                        {blogPostDetail.teaser}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </BoxedLayout>
+    </>
   );
 };
 
@@ -48,12 +100,22 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async function () {
   const blogPostDetails: BlogPostDetail[] =
     blogPosts.entryCollection?.items.map((item) => ({
       title: item?.title || "Untitled",
+      teaser: item?.teaser || "Untitled",
+      publicationDate: item?.publicationDate || new Date(),
       id: item?.sys.id || "<nowhere>",
     })) || [];
 
+  const sortedBlogPostDetails = blogPostDetails.sort((a, b) => {
+    if (new Date(a.publicationDate) < new Date(b.publicationDate)) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
   return {
     props: {
-      blogPostDetails,
+      blogPostDetails: sortedBlogPostDetails,
     },
   };
 };
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,10 +1,14 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Image from "next/future/image";
+import Head from "next/head";
 import { ParsedUrlQuery } from "querystring";
+import { BoxedLayout } from "../../components/BoxedLayout";
 import { newContentfulClient } from "../../contentful/client";
 
 interface BlogPost {
   title: string;
   content: string;
+  publicationDate: Date;
 }
 
 interface BlogPostPageProps {
@@ -25,8 +29,33 @@ const BlogPostPage: NextPage<BlogPostPageProps> = ({
 }: BlogPostPageProps) => {
   return (
     <>
-      <h2>{blogPost.title}</h2>
-      <p>{blogPost.content}</p>
+      <Head>
+        <title>{`${blogPost.title} - Contentful + NextJS Spike`}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <BoxedLayout maxStretch="768px" className="pt-12 md:pt-24">
+        <div className="flex flex-col gap-8 sm:gap-16">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-4xl font-bold !leading-tight text-gray-900 dark:text-gray-200 sm:text-6xl">
+              {blogPost.title}
+            </h1>
+            <p className="text-sm font-medium text-gray-400 dark:text-gray-600">
+              {new Date(blogPost.publicationDate).toLocaleDateString("en-US")}
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-8 sm:gap-16">
+            <div className="relative aspect-video h-auto w-full min-w-[50%] overflow-hidden rounded">
+              <Image src="/images/cover2.png" alt="Post image" fill />
+            </div>
+
+            <BoxedLayout maxStretch="600px" className="!p-0">
+              <div className="prose">{blogPost.content}</div>
+            </BoxedLayout>
+          </div>
+        </div>
+      </BoxedLayout>
     </>
   );
 };
@@ -70,6 +99,7 @@ export const getStaticProps: GetStaticProps<
   const blogPost: BlogPost = {
     title: blogPostQueryResult.post?.title || "",
     content: blogPostQueryResult.post?.content || "",
+    publicationDate: blogPostQueryResult.post?.publicationDate || "",
   };
 
   return {
